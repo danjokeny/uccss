5957761407b7eb3a86c0c689d317b08ea65ee4fc
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-10-10 12:32:57 -0500

    Express & NodeMon
    app with express and nodemon installed and tested

---
+++ Server/index.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+app.use(function(req, res, next){
+  console.log('Request from ' + req.ip);
+  next();
+});
+
+app.get('/',function(req,res){
+	res.send('Hello World!');
+});
+
+app.get('/about', function(req, res){
+	console.log('about');
+	res.send('About Us!');
+});
+
+app.get('/about/directions', function(req, res){
+	res.send('How to Find Us on a map!');
+});
+
+
+app.use(function(req, res){
+	res.type('text/plan');
+	res.status(404);
+	res.send('Your Page was not found');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plan');
+	res.status(500);
+	res.send('500 Sever Error');
+});
+
+http.createServer(app).listen(3000, function(){
+	console.log('Express server listening on port ' + 3000);
+})
