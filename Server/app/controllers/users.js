7ebca05d0d2c57e60cacbfa90701ceb731271f47
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-10-17 15:07:52 -0500

    Create (POST) new user tested

--- Server/app/controllers/users.js
+++ Server/app/controllers/users.js
@@ -10,9 +10,20 @@ module.exports = function (app, config) {
         logger.log('info', 'Get all users');
         res.status(200).json({ message: 'Got All Users' })
     });
-
+    
     router.route('/users/:id').get(function (req, res, next) {
         logger.log('info', 'Get user %s', req.params.id);
         res.status(200).json({ message: 'Get user = ' + req.params.id });
     });
+    
+    router.route('/login').post(function (req, res, next) {
+        console.log(req.body);
+        var email = req.body.email
+        var password = req.body.password;
+        logger.log('info', 'Created(POST) new user ' + email );
+        var obj = { 'email': email, 'password': password };
+        res.status(201).json(obj);
+    });
+
+
 };
