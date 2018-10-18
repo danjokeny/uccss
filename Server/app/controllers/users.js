3a75ced32a3ee3dbcbd43ceff86777d4c22afaf3
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-10-18 14:46:48 -0500

    update works as post, not as put
    update works as post, not as put, need to fix

--- Server/app/controllers/users.js
+++ Server/app/controllers/users.js
@@ -25,5 +25,14 @@ module.exports = function (app, config) {
         res.status(201).json(obj);
     });
 
+    router.route('/update').put(function (req, res, next) {
+        console.log(req.body);
+        var email = req.body.email
+        var emailNew = req.body.email2;
+        logger.log('info', 'Updated(PUT) existing user ' + email + ' to ' + emailNew );
+        var obj = { 'Existing email': email, 'New Email': emailNew };
+        res.status(201).json(obj);
+    });  
+
 
 };
