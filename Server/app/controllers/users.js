928541a9ef9177591b8ddb201d0a0de35bc0f6aa
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-10-24 13:15:55 -0500

    Async Post called and inserted data
    Async Post called and inserted data

--- Server/app/controllers/users.js
+++ Server/app/controllers/users.js
@@ -3,6 +3,7 @@ var express = require('express'),
     router = express.Router(),
     logger = require('../../config/logger'),
     mongoose = require('mongoose'),
+    asyncHandler = require('express-async-handler'),
     User = mongoose.model('User');
 
 
@@ -46,7 +47,7 @@ module.exports = function (app, config) {
         res.status(201).json(obj);
     });
 
-    router.route('/createNew').post(function (req, res, next) {
+    /*router.route('/createNew').post(function (req, res, next) {
         //create new user request 
         //api Post with json passed in raw body
         logger.log('info','Create User(Post)');
@@ -59,42 +60,55 @@ module.exports = function (app, config) {
             .catch(err => {
                 return next(err);
             });
-    });
+    });*/
+        
+    //create new user request 
+    //api Post with json passed in raw body
+    router.post('/createNew', asyncHandler(async (req, res) => {
+        logger.log('info', 'Creating user Async Post');
+        var user = new User(req.body);
+        console.log(req.body);
+        await user.save()
+            .then(result => {
+                res.status(201).json(result);
+            })
+    }));
+        
 
-    router.route('/update/:oldEmail/:newEmail').put(function (req, res, next) {
-        //Update old email to new email 
-        //Sample: 
-        //  http://localhost:5000/api/update/oldEmail:dforero@uwm.edu/newEmail:danjokeny@gmail.com
-        logger.log('info', 'Get user %s', req.params.id);
-        var emailOld = req.params.oldEmail
-        var emailNew = req.params.newEmail;
-        res.status(200).json({
-            message: 'Update(PUT) existing user = ' + emailOld
-                + ' to now be = ' + emailNew
+        router.route('/update/:oldEmail/:newEmail').put(function (req, res, next) {
+            //Update old email to new email 
+            //Sample: 
+            //  http://localhost:5000/api/update/oldEmail:dforero@uwm.edu/newEmail:danjokeny@gmail.com
+            logger.log('info', 'Get user %s', req.params.id);
+            var emailOld = req.params.oldEmail
+            var emailNew = req.params.newEmail;
+            res.status(200).json({
+                message: 'Update(PUT) existing user = ' + emailOld
+                    + ' to now be = ' + emailNew
+            });
         });
-    });
 
-    router.route('/updatePW/:Email/:newPW').put(function (req, res, next) {
-        //change password on existing user
-        //Sample:
-        //http://localhost:5000/api/updatePW/Email:dforero@uwm.edu/newPW:12345
-        logger.log('info', 'Put user %s', req.params.Email + ' with new PW = ' + req.params.newPW);
-        var Email = req.params.Email
-        var newPW = req.params.newPW;
-        res.status(200).json({
-            message: 'Update(PUT) password for user = ' + Email
-                + 'to now be = ' + newPW
+        router.route('/updatePW/:Email/:newPW').put(function (req, res, next) {
+            //change password on existing user
+            //Sample:
+            //http://localhost:5000/api/updatePW/Email:dforero@uwm.edu/newPW:12345
+            logger.log('info', 'Put user %s', req.params.Email + ' with new PW = ' + req.params.newPW);
+            var Email = req.params.Email
+            var newPW = req.params.newPW;
+            res.status(200).json({
+                message: 'Update(PUT) password for user = ' + Email
+                    + 'to now be = ' + newPW
+            });
         });
-    });
 
-    router.route('/Delete/:Email/').delete(function (req, res, next) {
-        //Delete existing userid
-        //sample:
-        //http://localhost:5000/api/delete/Email:danjokeny@gmail.com
-        logger.log('info', 'Get user %s', req.params.Email);
-        var Email = req.params.Email
-        res.status(200).json({ message: 'Delete user = ' + Email });
-    });
+        router.route('/Delete/:Email/').delete(function (req, res, next) {
+            //Delete existing userid
+            //sample:
+            //http://localhost:5000/api/delete/Email:danjokeny@gmail.com
+            logger.log('info', 'Get user %s', req.params.Email);
+            var Email = req.params.Email
+            res.status(200).json({ message: 'Delete user = ' + Email });
+        });
 
 
-};
+    };
