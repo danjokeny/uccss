35b8ae649614b92224f15ddd1965003841ffb95c
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-10-24 13:36:06 -0500

    Get All & GetByID working

--- Server/app/controllers/users.js
+++ Server/app/controllers/users.js
@@ -10,20 +10,35 @@ var express = require('express'),
 module.exports = function (app, config) {
     app.use('/api', router);
 
-    router.route('/users').get(function (req, res, next) {
-        //Get All Users Request
+    router.get('/users', asyncHandler(async (req, res) => {
+        //Get All Users Async Request
         //Sample: http://localhost:5000/api/users/
-        logger.log('info', 'Get all users');
-        res.status(200).json({ message: 'Got All Users' })
-    });
-
+        logger.log('info','Get all users Async Request');
+        let query = User.find();
+        query.sort(req.query.order)
+        await query.exec().then(result => {
+            res.status(200).json(result);
+        })
+    })); 
+        
+/*
     router.route('/users/:id').get(function (req, res, next) {
         //Get specific User Request 
         //http://localhost:5000/api/users/id:danny
         console.log(req.body);
         logger.log('info', 'Get user %s', req.params.id);
         res.status(200).json({ message: 'Get user = ' + req.params.id });
-    });
+    });*/
+    router.get('/users/:id', asyncHandler(async (req, res) => {
+        //Get specific User Request 
+        //http://localhost:5000/api/users/id:danny
+        logger.log('info','Get specific user by id =  %s',req.params.id);
+        await User.findById(req.params.id).then(result => {
+            res.status(200).json(result); 
+        })
+    }));
+        
+
 
     router.route('/login').post(function (req, res, next) {
         //Get existing user request 
@@ -46,24 +61,19 @@ module.exports = function (app, config) {
         var obj = { 'New user email': email, 'password': password };
         res.status(201).json(obj);
     });
-
-    /*router.route('/createNew').post(function (req, res, next) {
-        //create new user request 
-        //api Post with json passed in raw body
-        logger.log('info','Create User(Post)');
-        var user = new User(req.body);
-        console.log(req.body);
-        user.save()
-            .then(result => {
-                res.status(201).json(result);
-            })
-            .catch(err => {
-                return next(err);
-            });
-    });*/
-        
+      
     //create new user request 
     //api Post with json passed in raw body
+    /*
+    Sample:
+    //http://localhost:5000/api/createNew
+    {
+        "fname" : "Amy",    
+        "lname" : "Vankauwenberg",    
+        "email" : "AmyV@nm.com",   
+        "password" : "987654321",
+        "role" : "admin"
+    }*/
     router.post('/createNew', asyncHandler(async (req, res) => {
         logger.log('info', 'Creating user Async Post');
         var user = new User(req.body);
@@ -75,40 +85,40 @@ module.exports = function (app, config) {
     }));
         
 
-        router.route('/update/:oldEmail/:newEmail').put(function (req, res, next) {
-            //Update old email to new email 
-            //Sample: 
-            //  http://localhost:5000/api/update/oldEmail:dforero@uwm.edu/newEmail:danjokeny@gmail.com
-            logger.log('info', 'Get user %s', req.params.id);
-            var emailOld = req.params.oldEmail
-            var emailNew = req.params.newEmail;
-            res.status(200).json({
-                message: 'Update(PUT) existing user = ' + emailOld
-                    + ' to now be = ' + emailNew
-            });
+    router.route('/update/:oldEmail/:newEmail').put(function (req, res, next) {
+        //Update old email to new email 
+        //Sample: 
+        //  http://localhost:5000/api/update/oldEmail:dforero@uwm.edu/newEmail:danjokeny@gmail.com
+        logger.log('info', 'Get user %s', req.params.id);
+        var emailOld = req.params.oldEmail
+        var emailNew = req.params.newEmail;
+        res.status(200).json({
+            message: 'Update(PUT) existing user = ' + emailOld
+                + ' to now be = ' + emailNew
         });
+    });
 
-        router.route('/updatePW/:Email/:newPW').put(function (req, res, next) {
-            //change password on existing user
-            //Sample:
-            //http://localhost:5000/api/updatePW/Email:dforero@uwm.edu/newPW:12345
-            logger.log('info', 'Put user %s', req.params.Email + ' with new PW = ' + req.params.newPW);
-            var Email = req.params.Email
-            var newPW = req.params.newPW;
-            res.status(200).json({
-                message: 'Update(PUT) password for user = ' + Email
-                    + 'to now be = ' + newPW
-            });
+    router.route('/updatePW/:Email/:newPW').put(function (req, res, next) {
+        //change password on existing user
+        //Sample:
+        //http://localhost:5000/api/updatePW/Email:dforero@uwm.edu/newPW:12345
+        logger.log('info', 'Put user %s', req.params.Email + ' with new PW = ' + req.params.newPW);
+        var Email = req.params.Email
+        var newPW = req.params.newPW;
+        res.status(200).json({
+            message: 'Update(PUT) password for user = ' + Email
+                + 'to now be = ' + newPW
         });
+    });
 
-        router.route('/Delete/:Email/').delete(function (req, res, next) {
-            //Delete existing userid
-            //sample:
-            //http://localhost:5000/api/delete/Email:danjokeny@gmail.com
-            logger.log('info', 'Get user %s', req.params.Email);
-            var Email = req.params.Email
-            res.status(200).json({ message: 'Delete user = ' + Email });
-        });
+    router.route('/Delete/:Email/').delete(function (req, res, next) {
+        //Delete existing userid
+        //sample:
+        //http://localhost:5000/api/delete/Email:danjokeny@gmail.com
+        logger.log('info', 'Get user %s', req.params.Email);
+        var Email = req.params.Email
+        res.status(200).json({ message: 'Delete user = ' + Email });
+    });
 
 
     };
