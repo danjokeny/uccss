9afdf48dabace80ad63f3765548c07fa645c4e66
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-12-01 14:34:10 -0600

    New/Edit user form working

--- client/src/modules/users.js
+++ client/src/modules/users.js
@@ -21,23 +21,62 @@ export class users {
       email: "",
       password: ""
     };
-    this.showUserEditForm = true;
+    //this.showUserEditForm = true;
+    this.openEditForm();
   };
 
   async save() {
     if (this.user && this.user.fname && this.user.lname
       && this.user.email && this.user.password)
       await this.users.saveUser(this.user);
+    await this.getUsers();
+    this.back();
   };
 
+  back() {
+    this.showUserEditForm = false;
+  };
+
+  async saveUser(user) {
+    let serverResponse;
+    if (user) {
+      if (user._id) {
+        serverResponse = await this.data.put(user, this.USER_SERVICE);
+      } else {
+        serverResponse = await this.data.post(user, this.USER_SERVICE);
+      };
+      return serverResponse;
+    };
+  };
+
+  async delete() {
+    if (this.user) {
+      await this.users.delete(this.user);
+      await this.getUsers();
+      this.back();
+    };
+  };
+
+  openEditForm() {
+    this.showUserEditForm = true;
+    setTimeout(() => { $("#firstName").focus(); }, 500);
+  };
+
+
   //lifecycle method upon page load
-  async activate(){
+  async activate() {
     await this.getUsers();
   };
-  
-  async getUsers(){
+
+  async getUsers() {
     await this.users.getUsers();
   };
+
+  //lifecycle method for loading icon
+  attached() {
+    feather.replace()
+  };
+
 
 };
 
