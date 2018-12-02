25fdfddb397b4559dd6ad1557dc5965ffed1ffae
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-12-02 09:35:02 -0600

    update helptickets working

--- Server/app/controllers/help-ticket.js
+++ Server/app/controllers/help-ticket.js
@@ -33,11 +33,14 @@ module.exports = function (app, config) {
     }));
 
     //Get All helpTickets (check for Status parameter passed)
+    //Sort on attribute passed
     //Sample: http://localhost:5000/api/helpTickets/
     router.get('/helpTickets', asyncHandler(async (req, res) => {
         logger.log('info', 'Get all helpTickets');
         let query = HelpTicket.find();
+        //Sort on ?order
         query.sort(req.query.order)
+            //Join with user table to get names
             .populate({ path: 'PersonID', model: 'User', select: 'lname fname ' })
             .populate({ path: 'OwnerID', model: 'User', select: 'lname fname ' });
 
@@ -69,6 +72,25 @@ module.exports = function (app, config) {
             console.log(result);
             res.status(200).json(result);
         })
+    }));
+
+    //Update existing helpticket row with json passed in raw body
+    //Sample:http://localhost:5000/api/helpTickets/5c037743a7bb2fb12ca389b7
+    /*{
+    "_id": "5c037743a7bb2fb12ca389b7",
+    "Title": "Mr.",
+    "PersonID": "5bd312221d702b16809db1bd",
+    "OwnerID":  "5c02f00eef1abf258882cc24",
+    "Status": "new",
+    "DateCreated": "2018-12-02T06:10:11.768Z"
+    }*/
+    router.put('/helpTickets', asyncHandler(async (req, res) => {
+        logger.log('info', 'Updating helpTickets');
+        await HelpTicket.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true })
+            .then(result => {
+                logger.log('info', 'Updated helpTicket =' + result);
+                res.status(200).json(result);
+            })
     }));
 
 };
